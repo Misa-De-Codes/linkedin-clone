e9bcb25ee30ca9eb8debd3f76bd16929dd512bd0
Misa-De-Codes <musaddikulislam007@gmail.com>
2025-04-15 23:20:09 +0530

    Day 1- learnt from video 1 to 7

---
+++ learning-vite/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = function App(){
+  return(
+    <>
+      <div className="bg-black p-2 ">
+        <h1 className="text-3xl text-gray-400 font-bold font-mono">hellow World</h1>
+        <p className="text-gray-500 font-mono">I'm Musaddik And I'm trying my best to learn webdev.</p>
+        <button className="border-1 bg-gray-400 rounded-md my-1 mx-50% p-1 hover:bg-gray-200 hover:tr">Click Me</button>
+      </div>
+    </>
+  )
+}
+
+export default App;
