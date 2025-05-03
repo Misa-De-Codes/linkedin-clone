5a220e14764affd72aee1fe152b0c745208d09be
Misa-De-Codes <musaddikulislam007@gmail.com>
2025-05-03 13:23:10 +0530

    New start

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,13 +1,10 @@
-import Counter from "./Counter";
-import Greet from "./Greet";
 
 
 const App = function App(){
   return(
     <>
       <h1 className="text-5xl font-mono">hellow world</h1>
-      <Greet name="Susie" age={"susie"} />
-      <Counter/>
+      
     </>
   )
 }
---
+++ vite/src/components/AnimeCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AnimeCard({ imageUrl, title, summery,   }) {
+  return (
+    <div className=''>
+        <img src={prop.img}/>
+        <div className="">
+            <h2>{title}</h2>
+        </div>
+    </div>
+  )
+}
+
+export default AnimeCard
