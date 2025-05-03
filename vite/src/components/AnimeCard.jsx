b04cf6a12ae24fc271b01259b207f461d0507874
Misa-De-Codes <musaddikulislam007@gmail.com>
2025-05-03 20:21:41 +0530

    new start

--- vite/src/components/AnimeCard.jsx
+++ vite/src/components/AnimeCard.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
-function AnimeCard({ imageUrl, title, summery,   }) {
-  return (
-    <div className=''>
-        <img src={prop.img}/>
-        <div className="">
-            <h2>{title}</h2>
+function AnimeCard({ imageUrl = null, title, rating, summery, }) {
+    return (
+        <div className='flex w-full '>
+            <img className='' src={imageUrl} />
+            <div className=''>
+                <h2 className=''>{title}</h2>
+                <h3 className=''>{rating}</h3>
+                <p className=''>{summery}</p>
+                <div className='flex '>
+                    <p>👍</p>
+                    <p>⭐</p>
+                    <p>🤣</p>
+                    <p>🥹</p>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AnimeCard
